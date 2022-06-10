345179b15073432de659361f8323c22509aa1ade
Ulric Wilfred <shaman.sir@gmail.com>
2022-06-10 10:45:52 +0200

    collect references to the controls for `dat.gui`

--- genui.datgui.js
+++ genui.datgui.js
@@ -4,36 +4,42 @@ const addProp = (gui, prop, state, actions, update) => {
     const def = prop.def;
     const property = prop.property || prop.name;
     const name = prop.name || prop.property;
+    let control;
     switch (prop.kind) {
         case 'select':
             state[property] = def.current;
-            gui.add(state, property, def.values).name(name).onFinishChange((val) => { update(property, val); });
-            // TODO: love
-            break;
+            control = gui.add(state, property, def.values).name(name).onFinishChange((val) => { update(property, val); });
+            return { property, control };
         case 'float': case 'int':
             state[property] = def.current;
-            gui.add(state, property, def.min, def.max, def.step).name(name).onFinishChange((val) => { update(property, val); });
-            break;
+            control = gui.add(state, property, def.min, def.max, def.step).name(name).onFinishChange((val) => { update(property, val); });
+            return { property, control };
         case 'text': case 'color':
             state[property] = def.current;
-            gui.add(state, property).name(name).onFinishChange((val) => { update(property, val); });
-            break;
+            control = gui.add(state, property).name(name).onFinishChange((val) => { update(property, val); });
+            return { property, control };
         case 'toggle':
             state[property] = def.current;
-            gui.add(state, property).name(name).onFinishChange((val) => { update(property, val); });
-            break;
+            control = gui.add(state, property).name(name).onFinishChange((val) => { update(property, val); });
+            return { property, control };
         case 'nest':
             const nestFolder = gui.addFolder(name);
-            prop.def.children.forEach(childProp => {
-                addProp(nestFolder, childProp, def.nest ? state[def.nest] : state, actions, update);
-            });
+            const pre_mapping = prop.def.children.map(childProp => addProp(nestFolder, childProp, def.nest ? state[def.nest] : state, actions, update));
             if (def.expand) {
                 nestFolder.open();
             }
-            break;
+            control = nestFolder;
+            let mapping = {}
+            pre_mapping.forEach((p) => {
+                if (p) {
+                    mapping[p.property] = { control : p.control };
+                }
+            });
+            return { property, control, children : mapping };
         case 'action':
             if (actions.hasOwnProperty(property)) {
-                gui.add(actions, property).name(name);
+                control = gui.add(actions, property).name(name);
+                return { property, control };
             } else {
                 console.warn('actions object doesn\'t have handler for ' + property);
             }
@@ -42,9 +48,10 @@ const addProp = (gui, prop, state, actions, update) => {
             const xyFolder = gui.addFolder(name);
             state[property]['x'] = def.x.current;
             state[property]['y'] = def.y.current;
-            xyFolder.add(state[property], 'x', def.x.min, def.x.max, def.x.step).name(name).onFinishChange((val) => { update(property, { x: val }); });
-            xyFolder.add(state[property], 'y', def.y.min, def.y.max, def.y.step).name(name).onFinishChange((val) => { update(property, { y: val }); });
-            break;
+            const x = xyFolder.add(state[property], 'x', def.x.min, def.x.max, def.x.step).name(name).onFinishChange((val) => { update(property, { x: val }); });
+            const y = xyFolder.add(state[property], 'y', def.y.min, def.y.max, def.y.step).name(name).onFinishChange((val) => { update(property, { y: val }); });
+            control = xyFolder;
+            return { property, control, children : { x : { control : x.control }, y : { control : y.control } } };
         default:
             console.warn('property was not handled, because of its unsupported kind', prop.kind, property);
             break;
@@ -55,10 +62,15 @@ const GenUI = {}
 
 GenUI.toDatGUI = (genui, state, actions, update) => {
     const gui = new dat.GUI();
-    console.log('Gen UI, version ', genui.version)
-    genui.root.forEach(prop => {
-        addProp(gui, prop, state, actions, update);
+    console.log('Gen UI, version ', genui.version);
+    const pre_mapping = genui.root.map(prop => addProp(gui, prop, state, actions, update));
+    let mapping = {}
+    pre_mapping.forEach((p) => {
+        if (p) {
+            mapping[p.property] = { control : p.control, children : p.children };
+        }
     });
+    return mapping;
 };
 
 GenUI.toDatGUI_ = (root, state, update) => {
