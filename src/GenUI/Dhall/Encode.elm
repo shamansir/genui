33856c3ccbbc104c9aa0c0386d6d65825020dea6
Ulric Wilfred <shaman.sir@gmail.com>
2022-07-18 22:53:16 +0200

    work on encoding DHALL

--- src/GenUI/Dhall/Encode.elm
+++ src/GenUI/Dhall/Encode.elm
@@ -16,97 +16,125 @@ import GenUI.Color as Color
 import GenUI.Gradient as Gradient
 
 
-type alias Dhall = Indented
+type Selects = Dict String G.SelectItem
 
 
-def : G.Def -> Dhall
-def d =
+type alias Dhall = ( Selects, Indented )
+
+
+fi : Int -> String
+fi i =
+    if i < 0 then String.fromInt i
+    else "+" ++ String.fromInt i
+
+
+ff : Float -> String
+ff f =
+    if f - toFloat (floor f) == 0 then
+        String.fromFloat f ++ ".0"
+    else String.fromFloat f
+
+
+q : String -> String
+q s = "\"" ++ s ++ "\""
+
+
+def : String -> G.Def -> ( List G.SelectItem, Indented )
+def name d =
     let
         intDef id =
-            [ ( 0, "integer number field, current value is " ++ String.fromInt id.current )
-            , ( 1, "minimum value is " ++ String.fromInt id.min )
-            , ( 1, "maximum value is " ++ String.fromInt id.max )
-            , ( 1, "and the steps are in amount of  " ++ String.fromInt id.step )
-            ]
+            ( []
+            ,
+                [ ( 0, "b.int " ++ q name ++ " " ++
+                    " { min = " ++ fi id.min ++
+                    " , max = " ++ fi id.max ++
+                    " , step = " ++ fi id.step ++
+                    " , current = " ++ fi id.current ++ "}" ) ]
+            )
 
         floatDef fd =
-            [ ( 0, "float number field, current value is " ++ String.fromFloat fd.current )
-            , ( 1, "minimum value is " ++ String.fromFloat fd.min )
-            , ( 1, "maximum value is " ++ String.fromFloat fd.max )
-            , ( 1, "and the steps are in amount of  " ++ String.fromFloat fd.step )
-            ]
+            ( []
+            ,
+                [ ( 0, "b.float " ++ q name ++ " " ++
+                    " { min = " ++ ff fd.min ++
+                    " , max = " ++ ff fd.max ++
+                    " , step = " ++ ff fd.step ++
+                    " , current = " ++ ff fd.current ++ "}" ) ]
+            )
 
         xyDef xyd =
-            ( 0, "XY field, where X is" )
-                :: indent (floatDef xyd.x)
-                ++ ( 0, "and Y is " )
-                :: indent (floatDef xyd.y)
+            ( []
+            ,
+                [ ( 0, "b.xy " ++ q name )
+                , ( 1, "{ x = " ++
+                    " { min = " ++ ff xyd.x.min ++
+                    " , max = " ++ ff xyd.x.max ++
+                    " , step = " ++ ff xyd.x.step ++
+                    " , current = " ++ ff xyd.x.current ++ " }"
+                    )
+                , ( 1, ", y = " ++
+                    " { min = " ++ ff xyd.y.min ++
+                    " , max = " ++ ff xyd.y.max ++
+                    " , step = " ++ ff xyd.y.step ++
+                    " , current = " ++ ff xyd.y.current ++ " }"
+                    )
+                , ( 1, "}" )
+                ]
+            )
 
         toggleDef td =
-            [ ( 0
-              , "toggle field, currently is "
-                    ++ (if td.current then
-                            "enabled"
+            ( []
+            ,
+                [ ( 0
+                , "b.toggle " ++ q name ++ " " ++
+                        (if td.current then
+                            "True"
 
                         else
-                            "disabled"
-                       )
-              )
-            ]
+                            "False"
+                        )
+                )
+                ]
+            )
 
         colorDef cd =
-            [ ( 0, "color field, current value is " ++ Color.toString cd.current ) ]
+            ( []
+            , [ ( 0, "b.color " ++ q name " (" ++ color cd.current ++ ")" ) ]
+            )
 
         textDef cd =
-            [ ( 0, "text field, current value is \"" ++ cd.current ++ "\"" ) ]
+            ( []
+            , [ ( 0, "b.text " ++ q name ++ " " ++ q cd.current ) ]
+            )
 
         actionDef ad =
-            ( 0, "action button with" )
-                :: indent (face ad.face)
+            ( []
+            , case ad.face of
+                G.Default -> [ ( 0, "b.action " ++ q name ) ]
+                _ ->
+                    [ ( 0, "b.with_face" )
+                    , ( 1, "(b.action " ++ q name ++ ")" )
+                    , ( 1, "(" ++ face ad.face ++ ")")
+                    ]
+            )
 
         selectDef sd =
-            [ ( 0, "it is selection box" )
-            , ( 1, "its current value is \"" ++ sd.current ++ "\"" )
-            , ( 1, "its possible values are: " )
-            ]
-                ++ (indent <| indexedList selectItem sd.values)
-                ++ [ ( 1
-                     , case sd.nestAt of
-                        Just p ->
-                            "nested at property \"" ++ p ++ "\""
-
-                        Nothing ->
-                            "not nested at any property"
-                     )
-                   , ( 1, "visually it is:" )
-                   ]
-                ++ indent (selectKind sd.kind)
+            ( sd.values
+            ,
+                [ ( 0, "b.select " ++ q name ++ " " ++ name ++ "_items " ++ q sd.current ) ]
+            )
 
         nestDef nd =
-            [ ( 0, "nested panel" )
-            , ( 1
-              , "by default it is "
-                    ++ (case nd.form of
-                            G.Expanded -> "expanded"
-                            G.Collapsed -> "collapsed"
-                       )
-              )
-            , ( 1, "its inner components are: " )
-            ]
-                ++ (indent <| indexedList property nd.children)
-                ++ [ ( 1
-                     , case nd.nestAt of
-                        Just p ->
-                            "nested at JS property \"" ++ p ++ "\""
-
-                        Nothing ->
-                            "not nested at any JS property (stored plain in the state)"
-                     )
-                   , ( 1, "its face is:" )
-                   ]
-                ++ indent (face nd.face)
-                ++ ( 1, "the shape of its panel is:" )
-                :: indent (nestShape nd.shape)
+            ( []
+            ,
+                [ ( 0, "b.nest" )
+                , ( 1, q name )
+                , ( 1, "( b.children" )
+                ]
+                ++ ({- List.indexedMap (\idx c -> ) <| -} List.map property nd.children)
+                [ ( 1, ")")
+                ]
+            )
 
         gradientDef gd =
             [ ( 0, "gradient editor, current value is " ++ Gradient.toString gd.current ) ]
@@ -121,7 +149,7 @@ def d =
     in
     case d of
         G.Ghost ->
-            [ ( 0, "ghost" ) ]
+            ( [], [ ( 0, "b.ghost " ++ name ) ] )
 
         G.NumInt id ->
             intDef id
@@ -294,20 +322,3 @@ toString : Dhall -> String
 toString =
     List.map (\( indent_, str ) -> (String.fromList <| List.repeat indent_ ' ') ++ str)
         >> String.join "\n"
-
-
-indexedList : (a -> Indented) -> List a -> Indented
-indexedList f =
-    List.indexedMap
-        (\idx ->
-            List.indexedMap
-                (\iidx ( indent_, str ) ->
-                    if iidx == 0 then
-                        ( indent_, String.fromInt idx ++ ". " ++ str )
-
-                    else
-                        ( indent_, str )
-                )
-                << f
-        )
-        >> List.concat
